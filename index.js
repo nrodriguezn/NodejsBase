81d6c30647bf37d9159efccde6967f2f10c23456
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-04 22:04:12 -0500

    iniciacion de las rutas a utilizar en index.js, falta refactorizar

--- index.js
+++ index.js
@@ -12,9 +12,28 @@ app.use(bodyParser.json())
 app.get('/hola', (req, res) =>{
   res.send({message: 'Hola Mundo'})
 })//devuelve un callback que es la peticion y la respuesta
+
 app.get('/holap/:name', (req, res) =>{
   res.send({send: `Hola ${req.params.name}!`})
 })
+
+app.get('/api/product', (req, res) => {
+
+})
+
+app.get('/api/product/:productid', (req,res)=>{
+
+})
+app.post('/api/product', (req, res) =>{
+
+})
+app.put('/api/product/:productId', (req, res)=>{
+
+})
+app.delete('/api/delete/:productId', (req, res) =>{
+  
+})
+
 
 
 app.listen(port, ()=>{
