0ff0facc7a0cb4a585a87e7f1ae12704160e8e92
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-07 22:04:52 -0500

    controllers/user, middlewares/auth, services/index

--- controllers/user.js
+++ controllers/user.js
@@ -11,14 +11,23 @@ function signUp (req, res) {
     //la contraseña no se almacena aqui por que hay una funcion de pre guardado
   })
   user.save((err) =>{
-    if(err) res.status(500).send({message: `Error al crear el usuario : ${err}`})
+    if(err) return res.status(500).send({message: `Error al crear el usuario : ${err}`})
 
-    return res.status(200).send({ token: service.createToken(user) })
+    return res.status(201).send({ token: service.createToken(user) })
   })
 }
 
 function signIn (req, res) {
+  User.find({ email: req.body.email }, (err, user) =>{
+    if (err) return res.status(500).send({message: err})
+    if (!user) return res.status(404).send({ message: 'No existe el ususario' })
 
+    req.user = user
+    return res.status(200).send({
+      message: 'Te has logeado correctamente',
+      token: service.createToken(user)
+    })
+  })
 }
 
 module.exports = {
