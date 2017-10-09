5f1a3d755ea525d346a2d33df73da47c3f8e10e3
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-09 13:57:30 -0500

    avance en master, se pasa a feature

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.engine('.hbs', hbs({
+  defaultLayout: 'default',
+  ext: 'hbs'
+}))
+app.set('view-engine', '.hbs')
 app.use('/api', api)
 
 module.exports = app
